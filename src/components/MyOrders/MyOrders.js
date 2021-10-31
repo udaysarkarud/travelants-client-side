1938ef9f9d05dc09451c643f5f3574cd40ce254a
udaysarkarud <udaysarkarud@gmail.com>
2021-10-31 20:18:07 +0600

    update blog

--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useEffect, useState } from 'react';
 import useAuth from '../../Hook/useAuth';
 import swal from 'sweetalert';
 
